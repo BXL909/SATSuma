6e194edd23e5432df4922252b595136ae1196e8c
BXL909 <108906757+BXL909@users.noreply.github.com>
2023-03-04 19:12:34 +0000

    made start on help/info screen

--- SATSuma/helpScreen.Designer.cs
+++ SATSuma/helpScreen.Designer.cs
@@ -28,12 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(helpScreen));
             this.panel22 = new System.Windows.Forms.Panel();
             this.lblHelpHeading = new System.Windows.Forms.Label();
             this.textBoxHelpText = new System.Windows.Forms.TextBox();
             this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnHelpTextDown = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnHelpTextUp = new System.Windows.Forms.Button();
             this.panel22.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel22
@@ -66,13 +73,14 @@
             this.textBoxHelpText.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBoxHelpText.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBoxHelpText.ForeColor = System.Drawing.Color.Silver;
-            this.textBoxHelpText.Location = new System.Drawing.Point(10, 54);
+            this.textBoxHelpText.Location = new System.Drawing.Point(0, 0);
             this.textBoxHelpText.Multiline = true;
             this.textBoxHelpText.Name = "textBoxHelpText";
             this.textBoxHelpText.ReadOnly = true;
-            this.textBoxHelpText.Size = new System.Drawing.Size(560, 721);
+            this.textBoxHelpText.Size = new System.Drawing.Size(495, 675);
             this.textBoxHelpText.TabIndex = 201;
             this.textBoxHelpText.TabStop = false;
+            this.textBoxHelpText.TextChanged += new System.EventHandler(this.textBoxHelpText_TextChanged);
             // 
             // btnExit
             // 
@@ -95,15 +103,83 @@
             this.btnExit.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.btnExit.UseVisualStyleBackColor = false;
             // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.textBoxHelpText);
+            this.panel1.Location = new System.Drawing.Point(13, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(523, 693);
+            this.panel1.TabIndex = 204;
+            // 
+            // btnHelpTextDown
+            // 
+            this.btnHelpTextDown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(66)))), ((int)(((byte)(51)))));
+            this.btnHelpTextDown.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(72)))), ((int)(((byte)(9)))));
+            this.btnHelpTextDown.FlatAppearance.BorderSize = 0;
+            this.btnHelpTextDown.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(92)))), ((int)(((byte)(29)))));
+            this.btnHelpTextDown.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(92)))), ((int)(((byte)(29)))));
+            this.btnHelpTextDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHelpTextDown.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelpTextDown.ForeColor = System.Drawing.Color.White;
+            this.btnHelpTextDown.Location = new System.Drawing.Point(542, 729);
+            this.btnHelpTextDown.Name = "btnHelpTextDown";
+            this.btnHelpTextDown.Size = new System.Drawing.Size(30, 30);
+            this.btnHelpTextDown.TabIndex = 207;
+            this.btnHelpTextDown.TabStop = false;
+            this.btnHelpTextDown.Text = "▼";
+            this.btnHelpTextDown.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnHelpTextDown.UseVisualStyleBackColor = false;
+            this.btnHelpTextDown.Click += new System.EventHandler(this.btnHelp_Click);
+            this.btnHelpTextDown.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnHelpTextDown_MouseDown);
+            this.btnHelpTextDown.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnHelpTextDown_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.panel2.Location = new System.Drawing.Point(503, 53);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(33, 719);
+            this.panel2.TabIndex = 208;
+            // 
+            // btnHelpTextUp
+            // 
+            this.btnHelpTextUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(66)))), ((int)(((byte)(51)))));
+            this.btnHelpTextUp.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(72)))), ((int)(((byte)(9)))));
+            this.btnHelpTextUp.FlatAppearance.BorderSize = 0;
+            this.btnHelpTextUp.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(92)))), ((int)(((byte)(29)))));
+            this.btnHelpTextUp.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(92)))), ((int)(((byte)(29)))));
+            this.btnHelpTextUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHelpTextUp.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelpTextUp.ForeColor = System.Drawing.Color.White;
+            this.btnHelpTextUp.Location = new System.Drawing.Point(542, 66);
+            this.btnHelpTextUp.Name = "btnHelpTextUp";
+            this.btnHelpTextUp.Size = new System.Drawing.Size(30, 30);
+            this.btnHelpTextUp.TabIndex = 209;
+            this.btnHelpTextUp.TabStop = false;
+            this.btnHelpTextUp.Text = "▲";
+            this.btnHelpTextUp.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnHelpTextUp.UseVisualStyleBackColor = false;
+            this.btnHelpTextUp.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnHelpTextUp_MouseDown);
+            this.btnHelpTextUp.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnHelpTextUp_MouseUp);
+            // 
             // helpScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
             this.CancelButton = this.btnExit;
-            this.ClientSize = new System.Drawing.Size(581, 784);
+            this.ClientSize = new System.Drawing.Size(584, 784);
+            this.Controls.Add(this.btnHelpTextUp);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnHelpTextDown);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.textBoxHelpText);
             this.Controls.Add(this.panel22);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
@@ -113,8 +189,9 @@
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.helpScreen_Paint);
             this.panel22.ResumeLayout(false);
             this.panel22.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -123,5 +200,10 @@
         private System.Windows.Forms.Label lblHelpHeading;
         private System.Windows.Forms.TextBox textBoxHelpText;
         private System.Windows.Forms.Button btnExit;
+        public System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnHelpTextDown;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnHelpTextUp;
     }
 }
--- SATSuma/helpScreen.cs
+++ SATSuma/helpScreen.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace SATSuma
 {
@@ -19,7 +21,7 @@ namespace SATSuma
 
         private void helpScreen_Load(object sender, EventArgs e)
         {
-
+            panel1.VerticalScroll.Visible = false;
             if (((SATSuma)this.Owner).GetPanelBlock().Visible == true)
             {
                 lblHelpHeading.Text = "Block";
@@ -28,6 +30,7 @@ namespace SATSuma
             if (((SATSuma)this.Owner).GetPanelBlockList().Visible == true)
             {
                 lblHelpHeading.Text = "Blocks";
+                textBoxHelpText.Text = "Displays 15 blocks starting from the current block height. Enter a block number between 0 (Genesis Block) and the current block height to jump to that point in time or page through the list. Select a row on the list to view the transactions within that block.\r\n\r\nDefinitions:\r\n\r\nDate/time (table) - \r\nHeight (table) - the block height/number of the block. \r\nTX count (table) - the number of transactions that are included within the block.\r\nSize (table) - the size of the block in MB.\r\nFee range (table) - the range of transaction fees within the block (sat/vB)\r\nMed. (table) - median fee.\r\nReward(BTC) (table) - the miner reward comprising the block subsidy plus all transaction fees for the block.\r\nHash - \r\nBlock weight - \r\nNonce - \r\nMiner/Pool - \r\nVersion - \r\nTotal fees - \r\nAverage fee - \r\nTotal inputs - \r\nTotal outputs - \r\nAverage TX size - \r\nTransactions in Mempool - \r\nTransactions in next block - \r\nMin/Max fee in next block - \r\nTotal fees in next block - \r\nAvg attempts to solve block - \r\nNext difficulty adjustment block - \r\nAvg time between blocks - \r\nNext difficulty adjustment - \r\nEst. hashrate - \r\nBlock reward - \r\nHalving block/remaining - \r\n";
             }
             if (((SATSuma)this.Owner).GetPanelAddress().Visible == true)
             {
@@ -37,16 +40,120 @@ namespace SATSuma
             if (((SATSuma)this.Owner).GetPanelBitcoinDashboard().Visible == true)
             {
                 lblHelpHeading.Text = "Bitcoin dashboard";
+                textBoxHelpText.Text = "Displays an overview of a variety of key market, usage, mining, etc metrics. The data is refreshed every minute by default. This can be altered in the settings.\r\n\r\nDefinitions:\r\n\r\n1 BTC / USD - the current value of 1 bitcoin denominated in US dollars ($).\r\n\r\nMarket cap (USD) - Bitcoin's market capitalisation i.e the total value of all issued bitcoin.\r\n\r\nChange from ATH - The percentage difference between the current value of bitcoin and its all time high (ATH) value.\r\n\r\n1 USD / Sats - the value of 1 US dollar ($) denominated in sats/satoshis.\r\n\r\nAll time high (USD) - the highest value, denominated in US dollars ($), that bitcoin has reached, along with the date that it occurred.\r\n\r\n24hr high/ 24hr low (USD) - the highest and lowest values of 1 bitcoin over a 24 hour period, denominated in US dollars ($).\r\n\r\nTotal BTC in circulation - the total amount of bitcoin issued so far. This number will increase with each newly mined block until it reaches the hard cap of 21,000,000.\r\n\r\nNumber of holding addresses - the number of unique addresses currently holding an amount of bitcoin.\r\n\r\nAvg transactions per block - the average number of transactions included in a block, calculated over the previous 100 blocks.\r\n\r\n24 hour number of blocks mined - the number of blocks mined in the preceding 24 hours. This number should target 144 (a block every 10 minutes) and will be influenced towards that target by periodical mining difficulty adjustments. \r\n\r\n24 hour transaction count - the total number of transactions that were included in all blocks mined over the previous 24 hours.\r\n\r\n24 hour number of bitcoin sent - the total amount of all bitcoin sent (on-chain) over the previous 24 hours.\r\n\r\nTransactions in Mempool - the number of transactions currently in the mempool waiting to be included in a newly mined block.\r\n\r\nMin / max fee in next block - the anticipated minimum and maximum fee that will be included in the next block.\r\n\r\nTotal fees in next block - the anticipated total amount of fees to be included in the next block.\r\n\r\nTransactions in next block - the anticipated amount of transactions to be included in the next block.\r\n\r\nAvg no. of attempts to solve block - the average number of attempts made by the collective work of all miners to solve a block.\r\n\r\nAverage time between blocks - the average time between blocks being mined. This should average 10 minutes, influenced by periodical mining difficullty adjustments.\r\n\r\nEst. hashrate - an estimate of the total computational power currently being used in the mining process.\r\n\r\nBlock of next difficulty adjustment - the block height at which the next difficulty adjustment will occur. This happens every 2016 blocks which is approximately every two weeks.\r\n\r\nNext difficulty adjustment - the anticipated change to the current mining difficulty, expressed as a percentage.\r\n\r\nBlock reward - the current amount of the reward paid by the protocol for newly mined blocks, in the form of newly issued bitcoin. \r\n\r\nNumber of discoverable nodes - the number of currently discoverable Bitcoin nodes. This number is not representative of the total number of nodes, which will be much higher.\r\n\r\nBlockchain size - the current size of the entire blockchain/ledger in gigabytes (GB).\r\n\r\nHalving block / blocks remaining - the block height at which the next 'halving' will occur, and the number of blocks remaining until that time. The halving is when the block reward is cut in half until the next halving occurs. A halving occurs every 210,000 blocks, approximately every four years. \r\n\r\nHalving date / secs remaining - the date at which the next halving will occur, along with a countdown timer. \r\n\r\nBlock reward after halving - the value of the block reward once the next halving has taken place.";
             }
             if (((SATSuma)this.Owner).GetPanelLightningDashboard().Visible == true)
             {
                 lblHelpHeading.Text = "Lightning dashboard";
             }
+            
         }
 
         private void helpScreen_Paint(object sender, PaintEventArgs e)
         {
+
             ControlPaint.DrawBorder(e.Graphics, ClientRectangle, Color.Gray, ButtonBorderStyle.Solid);
+        }
+
+        private void ResizeTextBox(TextBox textBox)
+        {
+            Size textSize = TextRenderer.MeasureText(textBox.Text, textBox.Font, new Size(textBox.Width, int.MaxValue), TextFormatFlags.WordBreak);
+            textBox.Height = textSize.Height;
+        }
+
+        private void textBoxHelpText_TextChanged(object sender, EventArgs e)
+        {
+            ResizeTextBox(textBoxHelpText);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Set the focus to the TextBox control
+            textBoxHelpText.Focus();
+
+            // Simulate pressing the Up arrow key
+            SendKeys.Send("{UP}");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Set the focus to the TextBox control
+            textBoxHelpText.Focus();
+
+            // Simulate pressing the Up arrow key
+            SendKeys.Send("{DOWN}");
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            // Set the focus to the TextBox control
+            //textBoxHelpText.Focus();
+
+            // Simulate pressing the Up arrow key
+            //SendKeys.Send("{DOWN}");
+
+            panel1.VerticalScroll.Value++;
+        }
+        private bool isButtonPressed = false;
+        private bool downButtonPressed = false;
+        private bool upButtonPressed = false;
+
+        private void btnHelpTextDown_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            isButtonPressed = true;
+            downButtonPressed = true;
+            timer1.Start();
+        }
+
+        private void btnHelpTextUp_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            isButtonPressed = true;
+            upButtonPressed = true;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isButtonPressed)
+            {   if (downButtonPressed)
+                {
+                    if (panel1.VerticalScroll.Value < panel1.VerticalScroll.Maximum - 4)
+                    {
+                        panel1.VerticalScroll.Value = panel1.VerticalScroll.Value + 4;
+                    }
+                    timer1.Interval = 2; // set a faster interval while the button is held down
+                }
+                else if (upButtonPressed)
+                {
+                    if (panel1.VerticalScroll.Value > panel1.VerticalScroll.Minimum + 4)
+                    {
+                        panel1.VerticalScroll.Value = panel1.VerticalScroll.Value - 4;
+                    }
+                    timer1.Interval = 2; // set a faster interval while the button is held down
+                }
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void btnHelpTextDown_MouseUp(object sender, MouseEventArgs e)
+        {
+            isButtonPressed = false;
+            downButtonPressed = false;
+            timer1.Stop();
+            timer1.Interval = 50; // reset the interval to its original value
+        }
+
+        private void btnHelpTextUp_MouseUp(object sender, MouseEventArgs e)
+        {
+            isButtonPressed = false;
+            upButtonPressed = false;
+            timer1.Stop();
+            timer1.Interval = 50; // reset the interval to its original value
         }
     }
 }
